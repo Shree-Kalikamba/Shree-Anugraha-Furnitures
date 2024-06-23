4ea6ec493a55e3235376e2ec04e18440c4292824
Shree-Kalikamba <spark1by1@gmail.com>
2024-06-23 11:15:14 +0530

    Add files via upload

--- Search.js
+++ Search.js
@@ -2,20 +2,20 @@
 function Search(){
 
 var SearchVal= document.getElementById('SearchTF').value;
-if(SearchVal=="phone number")
+if(SearchVal=="Phone number"||SearchVal=="Phone number "||SearchVal=="phone number")
 {
 alert("Radhakrishna Achar: 9481518240");
 }
-else if(SearchVal.substring(0,2)=="so")
+else if(SearchVal.substring(0,2)=="so"||SearchVal.substring(0,2)=="So")
 {
 window.location.href="sofa.html";
 }
-else if(SearchVal.substring(0,2)=="te")
+else if(SearchVal.substring(0,2)=="te"||SearchVal.substring(0,2)=="Te")
 {
 window.location.href="teapoy.html";
 
 }
-else if(SearchVal.substring(0,2)=="do")
+else if(SearchVal.substring(0,2)=="do"||SearchVal.substring(0,2)=="Do")
 {
 window.location.href="doors.html";
   
